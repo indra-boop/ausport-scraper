d73cfe4d10755526dcb733eff7d3f8dae5f86d67
Indra Harmoyo <indra@jerconetwork.com>
2026-09-25 04:00:14 +0800

    fix: assemble scraper from text parts (replace corrupt gzip pack)

--- scraper.js
+++ scraper.js
@@ -1,10 +1,13 @@
 'use strict';
 const fs = require('fs');
 const path = require('path');
-const zlib = require('zlib');
 const assembled = path.join(__dirname, '.scraper.assembled.js');
 if (!fs.existsSync(assembled) || process.env.AUSPORT_REBUILD === '1') {
-  const b64 = fs.readFileSync(path.join(__dirname, 'scraper.js.gz.b64'), 'utf8');
-  fs.writeFileSync(assembled, zlib.gunzipSync(Buffer.from(b64, 'base64')));
+  const n = 6;
+  let body = '';
+  for (let i = 0; i < n; i++) {
+    body += fs.readFileSync(path.join(__dirname, 'scraper.part' + i + '.js.txt'), 'utf8');
+  }
+  fs.writeFileSync(assembled, body);
 }
 module.exports = require(assembled);
