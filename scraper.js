f99c448b673e1f6f3e173410e02415293132a770
Indra Harmoyo <indra@jerconetwork.com>
2026-09-25 03:58:37 +0800

    fix: restore scraper body (gzip) with description-channel merge for Fox League / Channel 7 / F1

--- scraper.js
+++ scraper.js
@@ -1,17 +1,10 @@
 'use strict';
 const fs = require('fs');
 const path = require('path');
+const zlib = require('zlib');
 const assembled = path.join(__dirname, '.scraper.assembled.js');
 if (!fs.existsSync(assembled) || process.env.AUSPORT_REBUILD === '1') {
-  const parts = [
-    require('./scraper-chunks/part0.js'),
-    require('./scraper-chunks/part1.js'),
-    require('./scraper-chunks/part2.js'),
-    require('./scraper-chunks/part3.js'),
-    require('./scraper-chunks/part4.js'),
-    require('./scraper-chunks/part5.js'),
-    require('./scraper-chunks/part6.js')
-  ];
-  fs.writeFileSync(assembled, parts.join(''));
+  const b64 = fs.readFileSync(path.join(__dirname, 'scraper.js.gz.b64'), 'utf8');
+  fs.writeFileSync(assembled, zlib.gunzipSync(Buffer.from(b64, 'base64')));
 }
 module.exports = require(assembled);
